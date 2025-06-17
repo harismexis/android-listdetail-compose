7a4ae675ff175edbabb311957f99fc47e80bafdf
Haris Mexis <theoharismexis@hotmail.com>
2025-06-17 20:34:34 +0100

    cleanup

--- app/src/main/java/com/harismexis/listdetail/model/Model.kt
+++ app/src/main/java/com/harismexis/listdetail/model/Model.kt
@@ -9,22 +9,3 @@ data class Model(
     val title: String?, // "Flowing Barchan Sand Dunes on Mars"
     val url: String? // "https://apod.nasa.gov/apod/image/0904/sandmars_mro.jpg"
 )
-
-fun String?.extractYouTubeVideoId(): String? {
-    if (this == null) return null
-    if (!this.contains("youtube")) return null
-    val embed = "/embed/"
-    val startIndex: Int = this.lastIndexOf(embed) + embed.length
-    if (startIndex <= 0) return null
-    val endIndex: Int = this.indexOf("?")
-    if (endIndex <= 0) return null
-    return this.substring(startIndex, endIndex)
-}
-
-fun Model?.isImage(): Boolean? {
-    return this?.url?.startsWith("https://apod.nasa.gov/apod/image/") == true
-}
-
-fun Model?.isVideo(): Boolean? {
-    return this?.url?.startsWith("https://www.youtube.com/embed/") == true
-}
