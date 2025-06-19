f61adf5be23f4cb1d86c4ca0f738167fa767edd1
Haris Mexis <theoharismexis@hotmail.com>
2025-06-19 16:45:25 +0100

    Show image

--- app/src/main/java/com/harismexis/listdetail/screens/ListScreen.kt
+++ app/src/main/java/com/harismexis/listdetail/screens/ListScreen.kt
@@ -1,8 +1,15 @@
 package com.harismexis.listdetail.screens
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyListState
@@ -15,6 +22,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import coil.compose.SubcomposeAsyncImage
 import com.harismexis.listdetail.api.Character
 import com.harismexis.listdetail.api.genderOrEmpty
 import com.harismexis.listdetail.api.nameOrEmpty
@@ -47,7 +55,12 @@ fun ListView(
     items: List<Character>,
     listState: LazyListState,
 ) {
-    LazyColumn(state = listState) {
+    LazyColumn(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(16.dp),
+        state = listState,
+    ) {
         items(
             count = items.size,
             contentType = { index -> items[index] },
@@ -60,13 +73,34 @@ fun ListView(
 
 @Composable
 private fun ItemRow(item: Character) {
-    Column {
-        Text(text = item.nameOrEmpty())
-        Text(text = item.statusOrEmpty())
-        Text(text = item.speciesOrEmpty())
-        Text(text = item.typeOrEmpty())
-        Text(text = item.genderOrEmpty())
+    Row(
+        modifier = Modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically,
+    ) {
+        Column(
+
+        ) {
+            Text(text = item.nameOrEmpty())
+            Text(text = item.statusOrEmpty())
+            Text(text = item.speciesOrEmpty())
+            Text(text = item.typeOrEmpty())
+            Text(text = item.genderOrEmpty())
+        }
+        Spacer(modifier = Modifier.weight(1f))
+        Column(
+
+        ) {
+            SubcomposeAsyncImage(
+                modifier = Modifier.size(100.dp, 200.dp),
+                model = item.image,
+                contentDescription = "Translated description of what the image contains",
+                loading = { ImageLoadingView() },
+                // error = { ImageErrorView() },
+            )
+        }
     }
+
 }
 
 @Composable
@@ -76,6 +110,18 @@ private fun LoadingView() {
             .padding(16.dp)
             .fillMaxSize()
             .wrapContentSize(Alignment.Center)
+    ) {
+        CircularProgressIndicator()
+    }
+}
+
+@Composable
+private fun ImageLoadingView() {
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(260.dp),
+        contentAlignment = Alignment.Center
     ) {
         CircularProgressIndicator()
     }
