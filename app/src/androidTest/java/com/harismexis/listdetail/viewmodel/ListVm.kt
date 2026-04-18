777df6feefeb53c178a8fbcd224d6d82431abb26
Haris Mexis <theoharismexis@hotmail.com>
2026-04-18 22:41:43 +0100

    fix issue of error missing from test view model

--- app/src/androidTest/java/com/harismexis/listdetail/viewmodel/ListVm.kt
+++ app/src/androidTest/java/com/harismexis/listdetail/viewmodel/ListVm.kt
@@ -5,24 +5,33 @@ import androidx.lifecycle.viewModelScope
 
 import com.harismexis.listdetail.api.Character
 import com.harismexis.listdetail.repository.RemoteRepository
+import com.harismexis.listdetail.repository.RemoteRepositoryImpl
+import kotlinx.coroutines.flow.MutableSharedFlow
 
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asSharedFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
 
 // This ViewModel will override the actual one according to this post:
 // https://stackoverflow.com/questions/77626980/mocking-viewmodels-for-ui-testing-in-android
 
-class ListVm(private val repo: RemoteRepository = RemoteRepository()) : ViewModel() {
+class ListVm(private val repo: RemoteRepository = RemoteRepositoryImpl()) : ViewModel() {
 
     private val tag = "ListVm"
 
-    private val _models = MutableStateFlow<List<Character>?>(null)
+    private val _models = MutableStateFlow<List<Character>?>(emptyList())
     val models: StateFlow<List<Character>?> = _models.asStateFlow()
 
     private val _isLoading = MutableStateFlow<Boolean>(false)
     val isLoading: StateFlow<Boolean> = _isLoading.asStateFlow()
+
+    private val _errors = MutableSharedFlow<String>(
+        replay = 0,
+        extraBufferCapacity = 1
+    )
+    val error = _errors.asSharedFlow()
 
     fun updateData() {
         _isLoading.value = true
