0ab971cd96cb7b1203e2a496cf34fc6f217c84f3
Haris Mexis <theoharismexis@hotmail.com>
2026-05-07 22:04:00 +0100

    updates

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 ### Overview:
-A list detail application using Jetpack Compose.
-
+A list detail application using Rick and Morty API 
+https://rickandmortyapi.com/documentation
 
 ### Technologies:
-Kotlin, Jetpack Compose.
+Jetpack Compose, Kotlin, Android Studio
--- app/src/main/java/com/harismexis/listdetail/api/ApiResponse.kt
+++ app/src/main/java/com/harismexis/listdetail/api/ApiResponse.kt
@@ -1,8 +1,40 @@
 package com.harismexis.listdetail.api
 
+/**
+ * API response model for the Rick and Morty API.
+ * https://rickandmortyapi.com/documentation
+ * Example url:
+ * https://rickandmortyapi.com/api/character/?page=2
+ */
 data class ApiResponse(
     val info: Info?,
     val results: List<Character>?,
 )
+
+data class Character(
+    val id: Int?,
+    val name: String?,
+    val status: String?,
+    val species: String?,
+    val type: String?,
+    val gender: String?,
+    val image: String?
+)
+
+data class Info(
+    val count: Int,
+    val pages: Int,
+    val next: String?, // "https://rickandmortyapi.com/api/character/?page=2",
+    val prev: String?,
+)
+
+fun String?.getValueOrNa(): String {
+    return if (!isNullOrEmpty()) {
+        this
+    } else {
+        "N/A"
+    }
+}
+
 
 
