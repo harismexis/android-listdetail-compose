5e7f61b94d8e7b94760aa48ba85174a597796879
Haris Mexis <theoharismexis@hotmail.com>
2026-05-10 09:50:40 +0100

    updates

--- app/src/main/java/com/harismexis/listdetail/screens/ListScreen.kt
+++ app/src/main/java/com/harismexis/listdetail/screens/ListScreen.kt
@@ -1,9 +1,9 @@
 package com.harismexis.listdetail.screens
 
-import android.widget.Toast
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
@@ -12,23 +12,16 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
-import androidx.lifecycle.Lifecycle
-import androidx.lifecycle.compose.LocalLifecycleOwner
-import androidx.lifecycle.compose.collectAsStateWithLifecycle
-import androidx.lifecycle.repeatOnLifecycle
 import androidx.paging.LoadState
 import androidx.paging.compose.LazyPagingItems
 import androidx.paging.compose.collectAsLazyPagingItems
@@ -117,11 +110,10 @@ private fun ItemRow(
 
 @Composable
 private fun LoadingView() {
-    Column(
+    Box(
         modifier = Modifier
-            .padding(16.dp)
-            .fillMaxSize()
-            .wrapContentSize(Alignment.Center)
+            .fillMaxSize(),
+        contentAlignment = Alignment.Center,
     ) {
         CircularProgressIndicator()
     }
--- app/src/main/java/com/harismexis/listdetail/viewmodel/ListVm.kt
+++ app/src/main/java/com/harismexis/listdetail/viewmodel/ListVm.kt
@@ -1,24 +1,14 @@
 package com.harismexis.listdetail.viewmodel
 
-import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import androidx.paging.Pager
 import androidx.paging.PagingConfig
 import androidx.paging.cachedIn
-
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.asStateFlow
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.flow.MutableSharedFlow
-import kotlinx.coroutines.flow.asSharedFlow
-
 import com.harismexis.listdetail.api.Character
 import com.harismexis.listdetail.paging.CharactersPagingSource
 import com.harismexis.listdetail.repository.RemoteRepository
 import com.harismexis.listdetail.repository.RemoteRepositoryImpl
-import com.harismexis.listdetail.repository.Result
 
 class ListVm(private val repo: RemoteRepository = RemoteRepositoryImpl()) : ViewModel() {
 
