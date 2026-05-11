51041cb97fb07cb1cbb8a62088f8a6830009c797
Haris Mexis <theoharismexis@hotmail.com>
2026-05-11 05:33:42 +0100

    updates

--- app/src/main/java/com/harismexis/listdetail/screens/DetailScreen.kt
+++ app/src/main/java/com/harismexis/listdetail/screens/DetailScreen.kt
@@ -13,9 +13,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.unit.dp
-
 import coil.compose.SubcomposeAsyncImage
-
 import com.harismexis.listdetail.api.getValueOrNa
 import com.harismexis.listdetail.viewmodel.DetailVm
 
